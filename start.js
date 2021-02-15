60c345c3389256bbf153c14d99f9189f14f2c917
anguslauhk <62167374+anguslauhk@users.noreply.github.com>
2021-02-15 15:59:31 +0800

    DTL TTIS & M Train Lookup

--- start.js
+++ start.js
@@ -1,4 +1,8 @@
-var link = document.createElement("link")
-link.href = "https://fonts.googleapis.com/css?family=Rubik"
-link.rel = "stylesheet"
-document.head.appendChild(link)
+var link = document.createElement("link");
+link.href = "https://fonts.googleapis.com/css?family=Rubik";
+link.rel = "stylesheet";
+document.head.appendChild(link);
+var meta = document.createElement("meta");
+meta.name = "viewport";
+meta.content = "width=device-width, initial-scale=1.0";
+document.head.appendChild(meta);
